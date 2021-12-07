24b725ea53383ed8095c9d993f87c3eb9acdf7ac
Caio Alves Eustáquio <48813979+CaioEustaquio@users.noreply.github.com>
2021-12-07 18:10:59 -0300

    Add files via upload

--- projetoemodelagemdesistemasdesoftware/exotrip/public_html/php/reservar.php
+++ projetoemodelagemdesistemasdesoftware/exotrip/public_html/php/reservar.php
@@ -1,161 +1,128 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Painel</title>
-  <link rel="stylesheet" type="text/css" href="plugins/semantic/semantic.min.css">
-  <script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
-  <script type="text/javascript" src="plugins/semantic/semantic.min.js"></script>
-</head>
-<body>
-  <div class="ui secondary  menu" style="padding: 10px;">
-    <a href="painel" class="item">
-      Painel
-    </a>
-    <a href="reservar" class="item">
-      Reservar
-    </a>
-    <div class="right menu">
-      <a name="usuarioLogado" class="ui item">
-        <?php echo  UsuarioLogado::getNome();?>
-      </a>      
-      <a name="logout" class="ui item">
-        Logout
-      </a>
-    </div>
-  </div>
-  <div style="" class="ui grid">
-    <div class="sixteen wide column">
-      <section class="ftco-section contact-section">
-        <div class="container">
-          <div class="row d-flex mb-5 contact-info justify-content-center">
-              <!--
-            <div class="col-md-8">
-              <div class="row mb-5">
-                <div class="col-md-4 text-center py-4">
-                  <div class="icon">
-                    <span class="icon-map-o"></span>
-                  </div>
-                  <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
-                </div>
-                <div class="col-md-4 text-center border-height py-4">
-                  <div class="icon">
-                    <span class="icon-mobile-phone"></span>
-                  </div>
-                  <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
-                </div>
-                <div class="col-md-4 text-center py-4">
-                  <div class="icon">
-                    <span class="icon-envelope-o"></span>
-                  </div>
-                  <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
-                </div>
-              </div>
-            </div>
-              -->
-          </div>
-          <div class="row block-9 justify-content-center mb-5">
-            <div class="col-md-8 mb-md-5">
-              <h2 class="text-center">Preencha os dados da reserva</h2>
-              <form action="#" class="bg-light p-5 contact-form">
-                <div class="form-group">
-                  <label>Destinos</label>
-                  <select class="form-control" name="destino">
-                    <option value="">Selecionar</option>
-                    <option value="Arraial D ajuda">Arraial D ajuda</option>
-                    <option value="Bora Bora">Bora Bora</option>
-                    <option value="Caldas Novas">Caldas Novas</option>
-                    <option value="Fortaleza">Fortaleza</option>
-                    <option value="Florianópolis">Florianópolis</option>
-                    <option value="Gramado">Gramado</option>
-                    <option value="Ilha Santorini">Ilha Santorini</option>
-                    <option value="Lençóis Maranhenses">Lençóis Maranhenses</option>
-                    <option value="Maceió">Maceió</option>
-                    <option value="Natal">Natal</option>
-                    <option value="Porto de Galinhas">Porto de Galinhas</option>
-                    <option value="Rio de Janeiro">Rio de Janeiro</option>
-                  </select>
-                </div>
-                <div class="form-row">
-                  <div class="form-group col-md-6">
-                    <label for="inputdata">Check-in</label>
-                    <input type="date" class="form-control" placeholder="Data de entrada" name="checkin">
-                  </div>
-                  <div class="form-group col-md-6">
-                    <label for="inputdata">Check-out</label>
-                    <input type="date" class="form-control" placeholder="Data de saída" name="checkout">
-                  </div>
-                </div>
-                <div class="form-row">
-                  <div class="form-group col-md-4">
-                    <label for="inputadutos">Adultos</label>
-                    <input type="number" class="form-control" placeholder="0" name="qtdAdultos" min="0" max="10">
-                  </div>
-                  <div class="form-group col-md-4">
-                    <label for="inputadutos">Crianças</label>
-                    <input type="number" class="form-control" placeholder="0" name="qtdCriancas" min="0" max="10">
-                  </div>
-                  <div class="form-group col-md-4">
-                    <label for="inputadutos">Quartos</label>
-                    <input type="number" class="form-control" placeholder="0" name="qtQuartos" min="0" max="10">
-                  </div>
-                </div>
-                <div class="form-row">
-                  <div class="form-group col-md-6">
-                    <input type="text" class="form-control" placeholder="Nome" name="nome">
-                  </div>
-                  <div class="form-group col-md-6">
-                    <input type="text" class="form-control" placeholder="Sobrenome" name="sobreNome">
-                  </div>
-                </div>
-                <div class="form-group">
-                  <input type="email" class="form-control" placeholder="Email" name="email">
-                </div>
-                <div class="form-group">
-                  <input type="email" class="form-control" placeholder="Confirmar endereço de Email" name="confirmarEmail">
-                </div>
-                <div class="form-group">
-                  <label>Meios de pagamento</label>
-                  <select class="form-control" name="meioPgto">
-                    <option value="">Selecionar</option>
-                    <option value="Boleto">Boleto</option>
-                    <option value="Cartão de crédito">Cartão de crédito</option>
-                    <option value="Transferência bancária">Transferência bancária</option>
-                  </select>
-                </div>
-                <button class="big fluid ui blue button" name="submit">Solicitar reserva</button>
-                <button class="big fluid ui blue loading disabled button" name="loading"style="display: none;">Carregando...</button>
-              </form>
-            </div>
-          </div>
-<!--           <div class="row justify-content-center">
-            <div class="col-md-10">
-              <div id="map" class="bg-white"></div>
-            </div>
-          </div> -->
-        </div>
-      </section>
-    </div>
-  </div>
-  <script src="js/reservar.js"></script>
-  <script>
-    $("a[name=logout]").click(function(){
-      SwAlertConfirm.fire({
-        text: "Deseja sair do sistema?"
-      }).then((result) => {
-        if(result.value)
-        {
-          $.post(url+"/php/logout.php", (response)=>{
-            if(response.success){
-              location.href = url;
-            }else{
-              SwAlertError.fire({text:response.error});
-            }
-          });
-        }
-      });    
-    });
-  </script>  
-</body>
-</html>
+<?php
+header("Content-Type: application/json; charset=utf-8");
+require '../../lib/autoload.php';
+
+$mysqli = new Mysql_i();
+
+try{
+
+	if(!$mysqli->conectar()){
+		throw new Exception("Não foi possível se conectar ao banco de dados");
+	}	
+
+	UsuarioLogado::setConexao($mysqli);
+
+	if(!UsuarioLogado::get()){
+		throw new Exception("Não foi possível recuperar o usuario logado");
+	}
+
+	if(!isset($_POST['post']) || empty($_POST['post'])){
+		throw new Exception("Não foi possível realizar a operação desejada!");
+	}
+
+	parse_str($_POST['post'], $post);
+
+	if(empty($post["destino"])){
+		throw new Exception("Selecione o destino");
+	}
+
+	if(empty($post["checkin"])){
+		throw new Exception("Selecione a data de check-in");
+	}
+
+	if(empty($post["checkout"])){
+		throw new Exception("Selecione a data de check-out");
+	}
+
+	if($post["checkin"] > $post["checkout"]){
+		throw new Exception("A data do check-in não pode ser maior que a data do check-out");
+	}
+
+	if(empty($post["qtdAdultos"])){
+		throw new Exception("Selecione a quantidade de adultos");
+	}
+
+	if(empty($post["qtdCriancas"])){
+		throw new Exception("Selecione a quantidade de crianças");
+	}
+
+	if(empty($post["qtQuartos"])){
+		throw new Exception("Selecione a quantidade de quartos");
+	}
+
+	if(empty($post["nome"])){
+		throw new Exception("Preencha o campo nome");
+	}
+
+	if(empty($post["sobreNome"])){
+		throw new Exception("Preencha o campo sobrenome");
+	}
+
+	if(empty($post["email"])){
+		throw new Exception("Preencha o campo e-mail");
+	}
+
+	if(empty($post["confirmarEmail"])){
+		throw new Exception("Confirme o e-mail");
+	}
+
+	if($post['email'] <> $post['confirmarEmail']){
+		throw new Exception("O campo e-mail e confirmar e-mail devem ser idênticos");
+	}
+
+	if(empty($post["meioPgto"])){
+		throw new Exception("Selecione o meio de pagamento");
+	}
+
+	$nomeCompleto = sprintf("%s %s", $post["nome"], $post["sobreNome"]);
+	// $checkin = (new Date($post["checkin"])->format('d/m/Y'));
+	// $checkout = (new Date($post["checkout"])->format('d/m/Y'));
+
+  // $email_to = "caio.eustaquio@hotmail.com";
+  $email_to = sprintf("exotrip.project@outlook.com.br, %s", $post['email']);
+  $datenow = (new DateTime("now", new DateTimeZone('America/Sao_Paulo')))->format('d/m/Y H:i:s');
+		
+  // Email body content 
+	$htmlContent = "Olá, há uma nova solicitação de de hospedagem <a href='http://www.exotrip.orgfree.com'><b>exotrip.orgfree.com</b></a>!<br>
+  <br>Nome: <b>{$nomeCompleto}</b>
+  <br>E-mail: <b>{$post['email']}</b>
+  <br>Destino: <b>{$post['destino']}</b>
+  <br>Check-in: <b>{$post['checkin']}</b>
+  <br>Check-out: <b>{$post['checkout']}</b>
+  <br>Adultos: <b>{$post['qtdAdultos']}</b>
+  <br>Crianças: <b>{$post['qtdCriancas']}</b>
+  <br>Quartos: <b>{$post['qtQuartos']}</b>
+  <br>Data preenchimento: {$datenow}<br><br>
+  Mensagem automática do sistema, por favor não responda.";
+	 
+	// Header for sender info 
+	$headers = "From: Exotrip <naoresponda@exotrip.com.br>";
+	 
+	// Boundary  
+	$semi_rand = md5(time());  
+	$mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";  
+	 
+	// Headers for attachment
+	$headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
+	 
+	// Multipart boundary  
+	$message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" . 
+	"Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n";  
+
+	$message .= "--{$mime_boundary}--";
+	$returnpath = "-f naoresponda@exotrip.com.br"; 
+
+ 	$send = @mail($email_to, "Solicitação de hospedagem - {$post["destino"]} | Exotrip", $message, $headers, $returnpath);
+
+	if(!$send){
+		throw new Exception("Erro ao enviar e-mail de confirmação");
+	}  
+
+	$response = ["success" => true];
+
+}catch (Exception $e) {
+	$response = ["success" => false, "error" => $e->getMessage()];
+}finally{
+	echo json_encode($response);
+}
+?>
